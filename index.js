4975a32a3394620959e474ab8efb27d4201cde52
Shvet Kantibhai Anaghan <sh618812@dal.ca>
2023-02-24 19:57:17 -0400

    new code

--- index.js
+++ index.js
@@ -1,121 +1,109 @@
-const express = require('express');
-const axios = require('axios');
-const AWS = require('aws-sdk')
-const fs = require('fs');
+const express = require("express");
+const axios = require("axios");
+const AWS = require("aws-sdk");
+const fs = require("fs");
 const app = express();
-const port = 5001;
+const port = 5000;
 
 app.use(express.json());
 
 AWS.config.update({
-    region:'us-east-1',
-    accessKeyId: "ASIAY4OD6TTAUFSHEQFE",
-    secretAccessKey: "WQoQyiwPCCntSc0PotlxCwftDqHOTu9CLKveLtFN",
-    sessionToken: "FwoGZXIvYXdzEH8aDBjfI9Zm8KVzE9C1fyLAAfmhuHDskXU3Q6PRsI58Vaxa0kxmBIYxReRXCYE9sN8QOrS3j5DJPsJRhGX+KCaHiu+isIDunQ+BQItipc5b+Xt50IUe327FneRIP6Dv83d9izqJg7bNrtr62YOnGSToRhKhgCmVsw1Ub4Pb2bV0tBKtzHj+DR6I/kxzJBTnIg4UbIZH2Ht9SkGGHb+21eJbYU5tt8rSH+XTlrtGCq1wzlEkXNR+CSuNaDP0gfGxlRRdvADs4IziEXUcXIeY4/3MhCj7geOfBjItIEJf4kbsA9FSfgE+y2rU41l5nLwQpLPTTGr/B4aEe4Ksd+T1KJToLEZWEbqJ"
-})
+  region: "us-east-1",
+  accessKeyId: "ASIAY4OD6TTAS75Z6W6A",
+  secretAccessKey: "ooGLzIP+9mgJCDBiwEeEF257knidBcmWeaqSupsU",
+  sessionToken:
+    "FwoGZXIvYXdzEIn//////////wEaDPVnrxT2WuPmudr+4SLAASXQKSen/+a5LgiFQWOUjx1oPBCjqJ4iDeT8qpxTkAd9+w4XDlqArmZAMu3yBS85vUdO96EAKV0/iEl/DNw8GL9gg39TdlXXD/nEMIQFGcB1iPMyKLP1gm2QSMqBulHw8/UFlMBJhzNSdyZCZ68rd8Ii9ETJi/yzgQqMMCuaUFuhln/ws2mN+qzV7KRR23uJ5+C/JrVZ0gx0Ci/ON1y33S7q3AZRqnpo5KeKgLumQZm5heiLUYPuT1IFyxtw2yLe4yjJmuWfBjItSfikS92mT9sTxTNx2Bl8KggpnK41pxpw/XBOXVhxQqpWddEh0PRBdxMU5P1P",
+});
 
 const s3 = new AWS.S3();
 
 var bucketName = "shvet-try-1";
-var myKey = 'file.txt';
+var myKey = "file.txt";
 
 // app.get("/", (req, res) => {
 //     res.send("Hello World! My name is Shvet");
 // });
 
+const robURL = "http://52.91.127.198:8080/start";
 
-const robURL = 'http://52.91.127.198:8080/start';
-
-        axios({
-          method: "post",
-          url: robURL,
-          data:
-            {
-            banner: "B00917946",
-            ip: "54.160.102.179"
-            }
-        })
-        .then(function(res){
-          response.status(200).send(res.data)
-        })
-
-
+axios({
+  method: "post",
+  url: robURL,
+  data: {
+    banner: "B00917946",
+    ip: "35.153.52.64:5000",
+  },
+}).then(function (res) {
+  response.status(200).send(res.data);
+});
 
 // Create a file
-app.post("/storedata", (req, res)  => {
-    const newData = req.body.data;
-    const params = {Bucket: bucketName, Key: myKey, Body: newData};
-    s3.upload(params, function(err,data) {
-    if (err){
-        console.log(err);
-    }
-    else{
-        console.log("Success!");
+app.post("/storedata", (req, res) => {
+  const newData = req.body.data;
+  const params = { Bucket: bucketName, Key: myKey, Body: newData };
+  s3.upload(params, function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success!");
     }
     res.status(200);
     res.json({
-        "s3uri": "https://shvet-try-1.s3.amazonaws.com/file.txt"
+      s3uri: "https://shvet-try-1.s3.amazonaws.com/file.txt",
     });
+  });
 });
-});
-
 
 // append data in the file
-app.post("/appenddata", (req, res)  => {
+app.post("/appenddata", (req, res) => {
+  const appendContent = req.body.data;
 
-    const appendContent = req.body.data;
+  var getParams = {
+    Bucket: bucketName,
+    Key: myKey,
+  };
 
-    var getParams = {
-        Bucket: bucketName, 
-        Key: myKey
-    };
+  s3.getObject(getParams, function (err, data) {
+    if (err) console.log(err, err.stack);
+    else {
+      const updatedData = data.Body.toString() + appendContent;
+      const params = {
+        Bucket: bucketName,
+        Key: myKey,
+        Body: updatedData,
+      };
 
-    s3.getObject(getParams, function(err, data) {
-        if (err) console.log(err, err.stack);
+      s3.upload(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
         else {
-            const updatedData = data.Body.toString() + appendContent;
-            const params = {
-                Bucket: bucketName,
-                Key: myKey,
-                Body: updatedData};
-
-            s3.upload(params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else     {
-                    console.log('Success updated.');
-                   // successful response
-                }
-             });
+          console.log("Success updated.");
+          // successful response
         }
-        res.status(200);
-        res.json({
-            s3uri: "https://shvet-try-1.s3.amazonaws.com/file.txt"
-        });
-    }); 
+      });
+    }
+    res.status(200);
+    res.json({
+      s3uri: "https://shvet-try-1.s3.amazonaws.com/file.txt",
+    });
+  });
 });
 
 // delete file
-app.post("/deletefile", (req, res)  => {
+app.post("/deletefile", (req, res) => {
+  const deleteUrl = req.body.data;
 
-    const deleteUrl = req.body.data;
+  var params = { Bucket: bucketName, Key: myKey };
 
-
-    var params = {  Bucket: bucketName, Key: myKey };
-
-    s3.deleteObject(params, function(err, data) 
-    {
-        if (err) 
-        {
-            console.log(err, err.stack); // error
-        }  
-        else
-        {
-            console.log("Successfully Deleted");// deleted
-        }                
-    });
-    res.status(200);
+  s3.deleteObject(params, function (err, data) {
+    if (err) {
+      console.log(err, err.stack); // error
+    } else {
+      console.log("Successfully Deleted"); // deleted
+    }
+  });
+  res.status(200);
 });
 
 app.listen(port, () => {
-    console.log(`Your app listening on port ${port}`)
-})
+  console.log(`Your app listening on port ${port}`);
+});
